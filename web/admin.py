ce4d66b07c5d99d970d1e54627f8ff3274d5127f
madebydaniz <madebydaniz@gmail.com>
2017-06-06 09:28:37 +0200

    Add Income model

--- web/admin.py
+++ web/admin.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from web.models import Expense
+from web.models import Expense, Income
 
 admin.site.register(Expense)
+admin.site.register(Income)
